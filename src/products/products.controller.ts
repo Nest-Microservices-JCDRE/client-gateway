1573ff1d007b6c68af16efd891fb8fc5301939fc
Juan Carlos Del Rio <jcdre@hotmail.com>
2025-07-24 11:33:32 -0500

    implement RpcCustomExceptionFilter

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -10,8 +10,8 @@ import {
   Post,
   Query,
 } from '@nestjs/common'
-import { ClientProxy } from '@nestjs/microservices'
-import { firstValueFrom } from 'rxjs'
+import { ClientProxy, RpcException } from '@nestjs/microservices'
+import { catchError, firstValueFrom } from 'rxjs'
 import { PaginationDto } from 'src/common'
 import { PRODUCT_SERVICE } from 'src/config'
 
@@ -33,14 +33,20 @@ export class ProductsController {
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    try {
-      const product = await firstValueFrom(
-        this.productsClient.send({ cmd: 'find_one_product' }, { id }),
-      )
-      return product
-    } catch (error) {
-      throw new BadRequestException(error)
-    }
+    return this.productsClient.send({ cmd: 'find_one_product' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err)
+      }),
+    )
+
+    // try {
+    //   const product = await firstValueFrom(
+    //     this.productsClient.send({ cmd: 'find_one_product' }, { id }),
+    //   )
+    //   return product
+    // } catch (error) {
+    //   throw new RpcException(error)
+    // }
   }
 
   @Patch(':id')
